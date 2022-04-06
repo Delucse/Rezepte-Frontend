634192fe43d82fb724839f3866d16d5bc33b5de1
Delucse <46593742+Delucse@users.noreply.github.com>
2022-04-06 17:44:14 +0200

    resize images

--- src/pages/Pictures.js
+++ src/pages/Pictures.js
@@ -12,9 +12,9 @@ function Pictures() {
     });
   }, []);
 
-  return (pictures.map(pic => {
+  return (pictures.map((pic, index) => {
     return( 
-      <div style={{textAlign: 'left'}}>
+      <div style={{textAlign: 'left'}} key={index}>
         <div>{pic.description} (hochgeladen am {moment(pic.createdAt, moment.ISO_8601).format("DD.MM.YYYY HH:mm:ss")} Uhr)</div>
         <img src={`${process.env.REACT_APP_API_URL}/media/${pic.file}`} alt={pic.description} height="200px"/>
       </div>
