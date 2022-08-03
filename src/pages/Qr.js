ebc494dae7f369c3258036d53fa8b9ebce6d36dc
Delucse <46593742+Delucse@users.noreply.github.com>
2022-08-03 22:59:49 +0200

    stop stream

--- src/pages/Qr.js
+++ src/pages/Qr.js
@@ -1,100 +1,97 @@
 import React, { useEffect, useState } from 'react';
 
+import { useDispatch } from 'react-redux';
+import { snackbarMessage } from '../actions/messageActions';
+
 import { useNavigate } from 'react-router-dom';
 
 import QrReader from 'react-qr-reader';
-import Alert from '../components/Alert';
-import {
-    FormControl,
-    Select,
-    MenuItem,
-    InputAdornment,
-    Box,
-} from '@mui/material';
 
-import Icon from '@mdi/react';
-import { mdiCamera } from '@mdi/js';
+import Dialog from '../components/Dialog';
+import Alert from '../components/Alert';
+
+import { Box, Button } from '@mui/material';
 
 function Qr() {
+    const dispatch = useDispatch();
+
     const navigate = useNavigate();
 
-    // const [url, setUrl] = useState(null);
+    const [url, setUrl] = useState(null);
+    const [open, setOpen] = useState(false);
     const [progress, setProgress] = useState(true);
-    const [camera, setCamera] = useState(false);
-    const [devices, setDevices] = useState([]);
-    const [id, setId] = useState(null);
+    const [stream, setStream] = useState(null);
 
     useEffect(() => {
-        initial();
+        window.document.addEventListener(
+            'visibilitychange',
+            handleVisibilityChange
+        );
+        return () => {
+            window.document.removeEventListener(
+                'visibilitychange',
+                handleVisibilityChange
+            );
+        };
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [stream]);
 
     useEffect(() => {
         return () => {
-            if (window.localstream) {
-                const tracks = window.localstream.getTracks();
-                tracks.forEach((track) => {
+            if (stream) {
+                stream.getTracks().forEach((track) => {
                     track.stop();
                 });
-                window.localstream = null;
             }
         };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     });
 
-    const initial = async () => {
+    const startStream = async (constraints) => {
         if (
             'mediaDevices' in navigator &&
             'getUserMedia' in navigator.mediaDevices
         ) {
-            const deviceList = await getCameraSelection();
-            setDevices(deviceList);
-            if (deviceList && deviceList.length > 0) {
-                setId(deviceList[0].deviceId);
-                startStream({
-                    video: {
-                        deviceId: {
-                            exact: deviceList[0].deviceId,
-                        },
-                    },
+            const constraints = {
+                video: {
+                    facingMode: 'environment',
+                },
+            };
+            navigator.mediaDevices
+                .getUserMedia(constraints)
+                .then((str) => {
+                    setStream(str);
+                    var video = document.querySelector('video');
+                    video.srcObject = str;
+                    video.onloadedmetadata = () => {
+                        video.play();
+                    };
+                    setProgress(false);
+                })
+                .catch((err) => {
+                    setStream(null);
+                    setProgress(false);
                 });
-            } else {
-                setCamera(false);
-                setProgress(false);
-            }
         }
     };
 
-    const startStream = async (constraints) => {
-        navigator.mediaDevices
-            .getUserMedia(constraints)
-            .then((stream) => {
-                // window.localstream = stream;
-                var video = document.querySelector('video');
-                if (video.srcObject) {
-                    video.srcObject.getTracks().forEach((track) => {
-                        track.stop();
-                    });
-                }
-                video.srcObject = stream;
-                video.onloadeddata = function (e) {
-                    video.play();
-                    setCamera(true);
-                    setProgress(false);
-                };
-            })
-            .catch((err) => {
-                setCamera(false);
-                setProgress(false);
-                console.info(err);
+    const stopStream = () => {
+        setProgress(true);
+        var video = document.querySelector('video');
+        if (video) {
+            video.srcObject.getTracks().forEach((track) => {
+                track.stop();
             });
+        }
+        setStream(null);
     };
 
-    const getCameraSelection = async () => {
-        const devices = await navigator.mediaDevices.enumerateDevices();
-        const videoDevices = devices.filter(
-            (device) => device.kind === 'videoinput'
-        );
-        return videoDevices;
+    const handleVisibilityChange = () => {
+        if (!stream && document.visibilityState === 'visible') {
+            startStream();
+        } else {
+            stopStream();
+        }
     };
 
     const handleError = (err) => {
@@ -106,28 +103,44 @@ function Qr() {
             const url = window.location.origin;
             const pattern = new RegExp(url, 'g');
             if (pattern.test(data)) {
-                // setUrl(url);
                 navigate(data.replace(url, ''));
             } else {
-                // setUrl(
-                //     'Es ist nur eine URL gültig, die auf diese Webseite verweist.'
-                // );
+                stopStream();
+                setUrl(data);
+                setOpen(true);
             }
-        } else {
-            // setUrl('Es muss ein gültiger QR-Code vorgezeigt werden.');
         }
     };
 
-    const handleChange = (event) => {
-        setProgress(true);
-        setId(event.target.value);
-        startStream({
-            video: {
-                deviceId: {
-                    exact: event.target.value,
-                },
-            },
-        });
+    const cancel = () => {
+        startStream();
+        setOpen(false);
+    };
+
+    const copy = async () => {
+        if (navigator.clipboard) {
+            await navigator.clipboard.writeText(url);
+        } else {
+            var textArea = document.createElement('textarea');
+            textArea.value = url;
+            // Avoid scrolling to bottom
+            textArea.style.top = '0';
+            textArea.style.left = '0';
+            textArea.style.width = '0';
+            textArea.style.position = 'fixed';
+            document.body.appendChild(textArea);
+            textArea.focus();
+            textArea.select();
+            document.execCommand('copy');
+            document.body.removeChild(textArea);
+        }
+        dispatch(
+            snackbarMessage(
+                'QR-Code-Inhalt wurde erfolgreich in Zwischenablage gespeichert.',
+                `qr${Date.now()}`
+            )
+        );
+        cancel();
     };
 
     return 'mediaDevices' in navigator &&
@@ -154,52 +167,56 @@ function Qr() {
                     className="qrImageWrapper"
                 />
             </Box>
-            {!progress ? (
-                camera ? (
-                    devices.length > 1 ? (
-                        <div
-                            style={{
-                                maxWidth: '500px',
-                                width: '100%',
-                                margin: 'auto',
-                            }}
-                        >
-                            <FormControl fullWidth>
-                                <Select
-                                    value={id}
-                                    onChange={(e) => handleChange(e)}
-                                    sx={{ borderRadius: 0, height: '56px' }}
-                                    startAdornment={
-                                        <InputAdornment
-                                            sx={{
-                                                maxHeight: '56px',
-                                                height: '56px',
-                                            }}
-                                            position="start"
-                                        >
-                                            <Icon path={mdiCamera} size={1} />
-                                        </InputAdornment>
-                                    }
-                                >
-                                    {devices.map((device, index) => (
-                                        <MenuItem
-                                            value={device.deviceId}
-                                            key={index}
-                                        >
-                                            {device.label}
-                                        </MenuItem>
-                                    ))}
-                                </Select>
-                            </FormControl>
-                        </div>
-                    ) : null
-                ) : (
-                    <Alert
-                        error
-                        message="Der QR-Scanner wird leider nicht auf deinem Endgerät unterstützt. Benutze ein anderes Gerät, das eine Kamera besitzt."
-                    />
-                )
+            {!progress && !stream ? (
+                <Alert
+                    error
+                    message="Der QR-Scanner wird leider nicht auf deinem Endgerät unterstützt oder du hast die Berechtigung zur Nutzung der Kamera untersagt."
+                />
             ) : null}
+            <Dialog
+                open={open}
+                onClose={cancel}
+                maxWidth="sm"
+                fullWidth
+                closeIcon
+                title={
+                    url && url.includes('http')
+                        ? 'Link öffnen'
+                        : 'Text kopieren'
+                }
+                content={<div>{url}</div>}
+                actions={
+                    <div>
+                        <Button
+                            onClick={cancel}
+                            variant="outlined"
+                            sx={{ borderRadius: 0, mr: 1 }}
+                        >
+                            Abbrechen
+                        </Button>
+                        {url && url.includes('http') ? (
+                            <Button
+                                component="a"
+                                onClick={() => setOpen(false)}
+                                href={url}
+                                target="_blank"
+                                variant="contained"
+                                sx={{ borderRadius: 0 }}
+                            >
+                                Bestätigen
+                            </Button>
+                        ) : (
+                            <Button
+                                onClick={copy}
+                                variant="contained"
+                                sx={{ borderRadius: 0 }}
+                            >
+                                Bestätigen
+                            </Button>
+                        )}
+                    </div>
+                }
+            />
         </div>
     ) : (
         <Alert
